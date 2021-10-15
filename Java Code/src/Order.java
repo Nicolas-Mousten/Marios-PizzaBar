35b165fd52ef3ce358f33ee49b67cb884d66c0c7
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-10-15 12:40:57 +0200

    Saved everything from old rep

--- Java Code/src/Order.java
+++ Java Code/src/Order.java
@@ -4,25 +4,27 @@ import java.time.format.DateTimeFormatter;
 
 
 public class Order {//Ni
-    private int customerID;
+    private int customerID;     //initialize variables
     private Pizza[] productList;
     private String currentTimeFormatted;
     private String currentDateFormatted;
     private String productListString;
     private int combinedPrice;
-    public Order(int customerID, Pizza[] productList){
+    public Order(int customerID, Pizza[] productList){      //constructing constructor, whitout date and combined prize since they are automatic
         this.customerID = customerID;
         productListString = "\n";
-        for (int i = 0; i < productList.length; i++) {
+        for (int i = 0; i < productList.length; i++) {  //remakes the array into a long string for better showing of the input.
             productListString += (productList[i] +"\n");
         }
-        LocalDate currentDate = LocalDate.now();
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        String currentDateFormatted = currentDate.format(dateFormat);
+        LocalDate currentDate = LocalDate.now();                                    //get the local date/time
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");   //specify the layout of the date/time
+        String currentDateFormatted = currentDate.format(dateFormat);               //adds the structure for the information
 
-        LocalTime currentTime = LocalTime.now();
-        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
-        String currentTimeFormatted = currentTime.format(timeFormat);
+        LocalTime currentTime = LocalTime.now();                                    //get the local date/time
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");     //specify the layout of the date/time
+        String currentTimeFormatted = currentTime.format(timeFormat);               //adds the structure for the information
+
+
         this.currentDateFormatted = currentDateFormatted;
         this.currentTimeFormatted = currentTimeFormatted;
 
